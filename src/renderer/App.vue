f4cd424d94b525bc8862fe012fd801a22242da2a
k-water <625592890@qq.com>
2017-08-01 16:38:10 +0800

    first Initial

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-filesystem'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
